4923260c0326ca87b4b801a56419f22ccda1db22
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2026-02-11 13:03:24 +0100

    Remove basePath and assetPrefix for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/webybox" : "",
-  assetPrefix: isProd ? "/webybox/" : undefined,
   images: {
     unoptimized: true,
   },
